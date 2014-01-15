2d2b77e03b34e6d2b353f42f40ae2ed303e35836
Sam Lucidi <mansam@csh.rit.edu>
2014-01-15 10:26:19 -0500

    Updated version number.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='validator.py',
-    version='0.1.0',
+    version='0.2.0',
     author='Samuel "mansam" Lucidi',
     author_email="mansam@csh.rit.edu",
     packages=['validator'],
