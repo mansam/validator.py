18880ba8cf9a39da04879f608b7d681a9ec60049
Sam Lucidi <mansam@csh.rit.edu>
2014-01-15 00:05:27 -0500

    Got everything squared away for packaging.

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='validator.py',
+    version='0.1.0',
+    author='Samuel "mansam" Lucidi',
+    author_email="mansam@csh.rit.edu",
+    packages=['validator'],
+    url='http://pypi.python.org/pypi/validator.py/',
+    license='LICENSE',
+    description='A library for validating that dictionary values meet certain sets of parameters. Much like form validators, but for dicts.',
+    long_description=open('README').read()
+)
+
+
