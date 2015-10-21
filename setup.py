58759b97b567128da5554ef6c454aa0fa53ae17b
Samuel Lucidi <sam.lucidi@lacoda.com>
2015-10-21 11:49:46 -0400

    Add trove classifiers to setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,24 @@ from setuptools import setup
 
 setup(
     name='validator.py',
-    version='1.2.2',
+    version='1.2.3',
     author='Samuel "mansam" Lucidi',
-    author_email="mansam@csh.rit.edu",
+    author_email="sam@samlucidi.com",
     packages=['validator'],
     url='https://github.com/mansam/validator.py',
-    license='MIT',
     description='A library for validating that dictionary values meet certain sets of parameters. Much like form validators, but for dicts.',
-    long_description=open('README.rst').read()
+    long_description=open('README.rst').read(),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.0",
+        "Programming Language :: Python :: 3.1",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: Implementation :: PyPy"
+    ]
 )
--- validator/__init__.py
+++ validator/__init__.py
@@ -29,7 +29,7 @@ Author: Samuel Lucidi <sam@samlucidi.com>
 
 """
 
-__version__ = "1.2.2"
+__version__ = "1.2.3"
 
 import re
 from collections import namedtuple
